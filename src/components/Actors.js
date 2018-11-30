7c54d80185af5a0645c267417ef00de770fca71d
Edward Sparks <edd_sparks@hotmail.com>
2018-11-30 07:22:12 +0000

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,17 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor =>
+        <div key={actor.name}>
+          Actor Name: {actor.name}
+          <ul>
+            {actor.movies.map(movie =>
+              <li key={movie}>{movie}</li>
+            )}
+          </ul>
+        </div>
+       )}
     </div>
   );
 };
